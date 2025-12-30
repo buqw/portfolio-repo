c334c08324717ae02f03cd4a66c241433a013f99
Hassan Almisbah <hassanalmisbah4798@gmail.com>
2025-12-30 15:26:20 +0300

    Solving a problem

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -13,7 +13,7 @@ export default function Projects(props){
         <div id="allProj">
             <ProjTemp name = {"Sharara Crypto Currencies Wallet"} src={"https://sharara-repo.vercel.app/"} img={Sharara}/>
             <ProjTemp name = {"To Do List"} src={"https://todo-repo-sepia.vercel.app/"} img={todo}/>
-            <ProjTemp name = {"Ring Size Calculator"} src={"ring-size-repo.vercel.app/"} img={ring} />
+            <ProjTemp name = {"Ring Size Calculator"} src={"https://ring-size-repo.vercel.app/"} img={ring} />
         </div>
 
         </div>
